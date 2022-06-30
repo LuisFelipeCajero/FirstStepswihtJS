faedf4febda9a6097323ac6049cf5764f57b294d
Luis Felipe Cajero <99145300+LuisFelipeCajero@users.noreply.github.com>
2022-06-29 21:58:33 -0500

    Create Example01.js

---
+++ JavaScript/Example01.js
@@ -0,0 +1,6 @@
+var nombre = "Luis Felipe Cajero Martínez";
+var edad = 23;
+var pi = 3.141596;
+let altura = 1.73;
+const Primaria ="Emiliano Zapata";
+console.log (nombre, edad, pi, altura, Primaria)
