54be14b5266491e4f636060b6db82bf8e41eb928
Luis Felipe Cajero <99145300+LuisFelipeCajero@users.noreply.github.com>
2022-06-29 21:57:46 -0500

    Create truly_falsy.js

---
+++ JavaScript/truly_falsy.js
@@ -0,0 +1,15 @@
+//Ejemplos de boolean donde se devuelve un valor Falso
+
+Boolean(0) //False
+Boolean(null) //False
+Boolean(NaN) //False
+Boolean(undefined) //False
+Boolean(false) //False
+Boolean("") //False
+
+//Examples where boolean return true
+
+Boolean(1) //true for 1 or any number different 0
+Boolean("a") //true for any caracter or an space inside the string
+Boolean([]) //true even if the array is empty)
+Boolean({}) // True even if the object is empty
