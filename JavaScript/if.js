e97a8caf191166a18a4b009313652d2ee871987b
Luis Felipe Cajero <99145300+LuisFelipeCajero@users.noreply.github.com>
2022-06-30 22:20:58 -0500

    Create if.js

---
+++ JavaScript/if.js
@@ -0,0 +1,9 @@
+var edad = 18;
+
+if (edad ===18) {
+    console.log("Puedes votar por vez primera")
+} else if (edad > 18){
+    console.log("Puedes votar, eres mayor de edad")
+} else {
+    console.log("No puedes votar, eres menor de edad")
+}
