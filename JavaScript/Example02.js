066577c71d39e0dae960e48a6e35e04c06b38bd2
Luis Felipe Cajero <99145300+LuisFelipeCajero@users.noreply.github.com>
2022-06-30 22:21:49 -0500

    Create Example02.js

---
+++ JavaScript/Example02.js
@@ -0,0 +1,9 @@
+//Ejemplo02, tipos de variables.
+
+var grupo = 3;
+var alumnos = 20;
+var flores = 15;
+let días = 2;
+let total_de_flores = (grupo * alumnos * flores * días);
+
+console.log("Se necesitan" + total_de_flores + " flores para cubrir ambos días del evento")
