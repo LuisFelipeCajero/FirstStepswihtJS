8414ec1b1eab66ce9637df30da390bc77abf3825
Luis Felipe Cajero <99145300+LuisFelipeCajero@users.noreply.github.com>
2022-07-02 12:10:19 -0500

    Create gradosF.js

---
+++ JS exercises/gradosF.js
@@ -0,0 +1,10 @@
+//Ejercicio para convertir 90 grados Fahrenheit a grafos Celsius y Kelvin
+let k;
+let c;
+let f = 90;//Grados Fahrenheit
+c = (90 - 32) * (5/9); //Opareación para convertir grados fahrenheit a Celsius
+console.log('90 grados Faherenheit son igual a ' + c + ' grados Celsius'); //Impriendo grados celsius
+
+k = (90 - 32) * (5/9) + 273.15; //Operación para grados Kelvin
+
+console.log('90 grados Faherenheit son igual a ' + k + ' grados Kelvin') //Imprimiendo grados kelvin
