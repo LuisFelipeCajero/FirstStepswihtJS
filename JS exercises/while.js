9397ce71a8805d9d1c1137d2bb64f7b174bc9085
Luis Felipe Cajero <99145300+LuisFelipeCajero@users.noreply.github.com>
2022-07-07 12:40:32 -0500

    Create while.js

---
+++ JS exercises/while.js
@@ -0,0 +1,10 @@
+let gasoline = 100;
+
+while (gasoline > 0) {
+    console.log('Gasolina Restante ' + gasoline);
+    //Gasoline = Gasoline - 1
+    gasoline --;
+    
+}
+
+console.log('Ya no tiene Gasolina');
