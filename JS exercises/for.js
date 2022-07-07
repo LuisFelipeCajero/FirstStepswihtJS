fcfa8c3cd363b067d5f0505a5bf6e565c60a1217
Luis Felipe Cajero <99145300+LuisFelipeCajero@users.noreply.github.com>
2022-07-07 12:07:53 -0500

    Create for.js

---
+++ JS exercises/for.js
@@ -0,0 +1,13 @@
+//Imprimir la tabla del 10
+let base = 10;
+
+//Limite de la tabla
+let limite = 20;
+
+
+for (let tabla = 1; tabla <= limite; tabla++){
+
+    let res = 10 * tabla;
+    
+    console.log( base + ` x ${tabla} = ${res}`);
+}
