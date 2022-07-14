9d7056b71340d4fbf68adade5eddcb31932ec10f
Luis Felipe Cajero <99145300+LuisFelipeCajero@users.noreply.github.com>
2022-07-13 21:30:02 -0500

    Create triangle.js

---
+++ practicas/triangle.js
@@ -0,0 +1,7 @@
+//Calcule el área de un triángulo rectangulo que tiene de base 10 y de altura 5
+
+let base = 10;
+let altura = 5;
+let triangle = (base * altura) / 2;
+
+console.log("El área del trángulo rectangulo es de " + triangle)
