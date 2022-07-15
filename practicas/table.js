22a4788d44c2691cfa120b21e70814e91a052c5b
Luis Felipe Cajero <99145300+LuisFelipeCajero@users.noreply.github.com>
2022-07-14 19:24:37 -0500

    Create table.js

---
+++ practicas/table.js
@@ -0,0 +1,14 @@
+ //Realice un algoritmo, que permita la impresión en consola de las tablas
+
+ 
+
+for(let i = 1; i < 5; i++){
+   
+    let line = '';
+
+    for (let j = 1; j <= 5; j++){
+
+        line += `${j * i}  `
+    }
+    console.log(line);
+ }
